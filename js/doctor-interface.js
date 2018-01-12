33fed4193ac80b608c39dc2946ea289d00cf3e51
Amy Churchwell <amy.churchwell@gmail.com>
2018-01-12 09:12:32 -0800

    Debug apiKey error, returns vague result of doctor from request

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,13 +1,13 @@
 import { getData } from './../js/scripts.js';
-var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
   $('#dr-form').submit(function(event) {
     event.preventDefault();
     let name = $('#name-input').val();
+    let apiKey = require('./../.env').apiKey;
     $('#name-input').val("");
     getData(name, apiKey, function(response) {
-      $('#results').text(`Your result is ${response.data.profile.last_name}`);
+      $('#results').text(`Your result is ${response.meta.item_type}`);
     }, function() {
       $('#results').text("There was an error processing your request. Please try again.");
     });
