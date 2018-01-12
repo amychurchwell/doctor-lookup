8c99798fde0134eb3642670f769c7a1d5e619e99
Amy Churchwell <amy.churchwell@gmail.com>
2018-01-12 08:04:09 -0800

    Set up asset pipeline and initial directory

---
+++ README.md
@@ -0,0 +1,7 @@
+### Behavior-Driven Development Workflow
+
+https://www.learnhowtoprogram.com/javascript/bdd-with-javascript/the-red-green-refactor-workflow-12fb3e35-237a-4d6a-b398-2e05d518f023
+
+### When cloning:
+$ npm install
+$ bower install
---
+++ spec/boilerplate-spec.js
@@ -0,0 +1,21 @@
+import { Triangle } from './../js/constructor.js';
+
+describe('Triangle', function() {
+  var reusableTriangle;
+
+  beforeEach(function() {
+    reusableTriangle = new Triangle(5, 5, 5);
+  });
+
+  it('should show how beforeEach() works', function() {
+    console.log(reusableTriangle);
+  });
+
+  it('should test whether a Triangle has three sides', function() {
+    var triangle = new Triangle(3,4,5)
+    expect(triangle.side1).toEqual(3)
+    expect(triangle.side2).toEqual(4)
+    expect(triangle.side3).not.toEqual(6)
+  });
+
+});
