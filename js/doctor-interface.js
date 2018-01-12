b3e6a9227a57b6f462f29169243a3ffa5af7ea3a
Amy Churchwell <amy.churchwell@gmail.com>
2018-01-12 12:59:59 -0800

    Separate front-end and back-end logic for counter function

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -9,17 +9,9 @@ $(document).ready(function() {
     $('#name-input').val("");
     $('#results').empty();
     getData(name, apiKey, function(response) {
-      let i;
-      for (i = 0; i < 5; i++) {
-        $('#results').append(`i`);
-        $('#results').append(`
-          Your result is ${response.data[i].practices[0].name}<br>
-          Phone number: ${response.data[i].practices[0].phones[0].number}<br>
-          Accepts new patients? ${response.data[0].practices[0].accepts_new_patients}<br>
-          Address:<br> ${response.data[i].practices[0].visit_address.street}, Portland, OR.<br>
-          `);
-      }
       $('#results').append(`You received: ${response.meta.count} results.<br>`);
+      counter(response);
+      }
     }, function() {
       $('#results').text("There was an error processing your request. Please try again.");
     });
--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,15 @@ export function getData(search, apiKey, yes, no){
       }
     });
   }
+
+export function counter(){
+  let i;
+  for (i = 0; i < 5; i++) {
+    $('#results').append(`
+      Your result is ${response.data[i].practices[0].name}<br>
+      Phone number: ${response.data[i].practices[0].phones[0].number}<br>
+      Accepts new patients? ${response.data[0].practices[0].accepts_new_patients}<br>
+      Address:<br> ${response.data[i].practices[0].visit_address.street}, Portland, OR.<br>
+      `);
+    }
+  }
