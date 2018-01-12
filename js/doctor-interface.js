58b12ca0d56b8827ff66537a8315afd8222e0c76
Amy Churchwell <amy.churchwell@gmail.com>
2018-01-12 09:54:20 -0800

    Add results for Dr address, phone, and new patients.

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -3,13 +3,28 @@ import { getData } from './../js/scripts.js';
 $(document).ready(function() {
   $('#dr-form').submit(function(event) {
     event.preventDefault();
-    let name = $('#name-input').val();
-    let apiKey = require('./../.env').apiKey;
+    const apiKey = require('./../.env').apiKey;
+    const name = $('#name-input').val();
     $('#name-input').val("");
+    $('#results').empty();
     getData(name, apiKey, function(response) {
-      $('#results').text(`Your result is ${response.data[0].practices[0].name}`);
+      console.log(${response.data}.length);
+      $('#results').append(`
+        Your result is ${response.data[0].practices[0].name}<br>
+        Phone number: ${response.data[0].practices[0].phones[0].number}<br>
+        Accepts new patients? ${response.data[0].practices[0].accepts_new_patients}<br>
+        Address:<br> ${response.data[0].practices[0].visit_address.street}, Portland, OR.
+        `);
     }, function() {
       $('#results').text("There was an error processing your request. Please try again.");
     });
   });
 });
+
+// first name,
+// last name,
+// address,
+// phone number,
+//  website and
+//  whether or not the doctor is accepting new patients
+  for (i = 0; i < ${response.data})
