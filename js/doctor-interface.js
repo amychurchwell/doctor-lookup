38fed2253c31709c8e4173f3560fd19d1d6d622e
Amy Churchwell <amy.churchwell@gmail.com>
2018-01-12 08:26:31 -0800

    Add form and rough draft of UI JS

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,5 +1,8 @@
+import { getData } from './../js/scripts.js';
 var apiKey = require('./../.env').apiKey;
 
 $('document').ready(function(){
-
+  $('#dr-form').submit(function(event){
+    event.preventDefault();
+  });
 });
